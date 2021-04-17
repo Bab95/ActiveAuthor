0c704dccc4e2323adc513c888564ddf4ed839bfc
Captain_Levi_Ackerman <babnish95@gmail.com>
2021-04-17 11:58:52 +0530

    Removed unused variables and Fixed Method Name
    Removed unused variables and Fixed Method Name

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args){
-        List<String> ans = JSONReader.getusernames(10);
+        List<String> ans = JSONReader.getUsernames(10);
         for(String str : ans ){
             System.out.println(str);
         }
